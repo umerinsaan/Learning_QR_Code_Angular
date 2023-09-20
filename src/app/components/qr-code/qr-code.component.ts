dcb35027e25de303e2641005dc33be6d7b6b934e
umerinsaan <umerinsaan@gmail.com>
2023-09-20 16:11:27 +0500

    made some minor changes

--- src/app/components/qr-code/qr-code.component.ts
+++ src/app/components/qr-code/qr-code.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Order } from 'src/app/models/Order.model';
+import { QRService } from 'src/app/services/qr.service';
+import * as QRCode from 'qrcode-generator';
 
 @Component({
   selector: 'app-qr-code',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./qr-code.component.css']
 })
 export class QRCodeComponent {
+  order!: Order;
 
+  constructor(private qrService: QRService) {
+    this.order = qrService.getOrder();
+  }
+
+  generateQRCode(url: string) {
+    const typeNumber = 4;
+    const errorCorrectionLevel = 'H';
+
+    const qr = QRCode(typeNumber, errorCorrectionLevel);
+    qr.addData(url);
+    qr.make();
+
+    const qrCodeDataUrl = qr.createDataURL(10, 0);
+
+    return qrCodeDataUrl;
+  }
 }
